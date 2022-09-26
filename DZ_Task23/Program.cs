0e83117225900915eb10f74db7dd916ab7712624
alexandr <chochumakov_alek@mail.ru>
2022-09-26 19:43:06 +0300

    DZ_Task23 is ready

--- DZ_Task23/Program.cs
+++ DZ_Task23/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void TableCube(int num)
+{
+    Console.Write($"{num} ->");
+    for (int i = 1; i < num + 1; i++)
+    {
+        int cube = Convert.ToInt32(Math.Pow(i, 3));
+        if (i == num) Console.Write($" {cube}");
+        else Console.Write($" {cube},");
+    }
+}
+
+Console.WriteLine("Введите положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) Console.WriteLine("Вы ввели отрицательное число, введите положительное число: ");
+else TableCube(number);
